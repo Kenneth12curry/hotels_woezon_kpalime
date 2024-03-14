f3bb8f4b988d91f3b297edfedb3a86a7b65dd0d4
kenneth <kenneth.adjete@ism.edu.sn>
2024-03-14 15:46:36 +0000

    sixth  commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 
 from application import app
-
+import os
 # from flask import Flask 
 
 # app = Flask(__name__)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
